785020903e0efa4232935443e0f7e5e3cbd07ff4
Kittikorn01 <kittikornt01@gmail.com>
2026-06-09 15:18:46 +0700

    Lesson 4

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,24 +8,45 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
     private float horizontalInput;
-
+    private PlayerInputActions inputActions;
+    private Vector2 moveInput;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        inputActions = new PlayerInputActions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Jump.performed += OnJump;
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Jump.performed -= OnJump;
+        inputActions.Player.Disable();
+
     }
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(Vector2.up * playerData.jumpForce, ForceMode2D.Impulse);
-        }
+        moveInput = inputActions.Player.Move.ReadValue<Vector2>();
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(horizontalInput * playerData.moveSpeed, rb.linearVelocity.y);
+        rb.linearVelocity = new Vector2(
+            moveInput.x * playerData.moveSpeed,
+            rb.linearVelocity.y
+        );
+    }
+
+    void OnJump(InputAction.CallbackContext context)
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(Vector2.up * playerData.jumpForce, ForceMode2D.Impulse);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
